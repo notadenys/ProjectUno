90ec7bf7e218e044355b45a1af65d77811ac3a0e
notadenys <sh.denys.ev@gmail.com>
2023-12-06 15:30:27 +0100

    initialized a game

--- src/uno/jeu/Joueur.java
+++ src/uno/jeu/Joueur.java
@@ -1,11 +1,23 @@
 package uno.jeu;
 
+import uno.cartes.FabriqueCartes;
+import uno.cartes.PaquetDeCartes;
+
 public abstract class Joueur {
     private String nom;
+    private PaquetDeCartes main;
 
     public Joueur(String nom)
     {
         this.nom = nom;
+
+        FabriqueCartes fc = FabriqueCartes.getInstance();
+        main = fc.getPaquetVide();
+    }
+
+    public PaquetDeCartes getMain()
+    {
+        return main;
     }
 
     String getNom()
--- src/uno/jeu/Uno.java
+++ src/uno/jeu/Uno.java
@@ -1,7 +1,75 @@
 package uno.jeu;
 
-public class Uno {
-    public Uno(){
+import uno.cartes.FabriqueCartes;
+import uno.cartes.PaquetDeCartes;
 
+import java.util.ArrayList;
+
+public class Uno {
+    private boolean sens;
+
+    private int joueurQuiJoue;
+    private int joueurQuiDistribue;
+
+    private ArrayList<Joueur> joueurs;
+    private PaquetDeCartes talon;
+    private PaquetDeCartes pioche;
+
+    public Uno(){}
+
+    public void initialiser(int nbBots)
+    {
+        sens = true;
+
+        creerLesJoueurs(nbBots);
+        choisirQuiDistribue();
+        choisirQuiJoue();
+        distribuerCartes();
+    }
+
+    public void creerLesJoueurs(int nbBots)
+    {
+        assert nbBots >= 1 : "Pas de bots";
+        joueurs = new ArrayList<Joueur>(nbBots + 1);
+        joueurs.add(new JoueurHumain("xx"));
+        for (int i = 0; i < nbBots; i++)
+        {
+            joueurs.add(new Bot("Bot " + (i + 1)));
+        }
+    }
+
+    int getNbJoueurs()
+    {
+        return joueurs.size();
+    }
+
+    int getJoueurQuiDistribue()
+    {
+        return joueurQuiDistribue;
+    }
+
+    public void choisirQuiDistribue() {
+        joueurQuiDistribue = (int)(Math.random() * getNbJoueurs());
+    }
+
+    public void choisirQuiJoue() {
+        joueurQuiJoue = getJoueurQuiDistribue() == getNbJoueurs() ? 0 : getJoueurQuiDistribue() + 1;
+    }
+
+    public void distribuerCartes()
+    {
+        FabriqueCartes singleton = FabriqueCartes.getInstance();
+        talon = singleton.getPaquetVide();
+        pioche = singleton.getPaquetUno();
+        pioche.melanger();
+
+        for(Joueur j : joueurs)
+        {
+            for (int i = 0; i < 7; i++)
+            {
+                j.getMain().ajouter(pioche.piocher());
+            }
+        }
+        talon.ajouter(pioche.piocher());
     }
 }
