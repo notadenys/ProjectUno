adcb3f80858d4ecd9100af2fdb64f4302c23d703
notadenys <sh.denys.ev@gmail.com>
2023-12-29 17:15:22 +0100

    ajoute dialogue ligne de commande

---
+++ src/uno/jeu/DLC.java
@@ -0,0 +1,39 @@
+package uno.jeu;
+
+// Dialogue ligne de commande
+public class DLC {
+    private Uno uno;
+
+    public DLC(Uno u)
+    {
+        this.uno = u;
+        this.uno.setDLC(this);
+    }
+
+    public void reagir()
+    {
+        System.out.println("\nLa carte sur top est " + uno.getTopCarte().getAffichage());
+
+        System.out.println("C'est le tour du " + uno.getNomDeJoueurCourant() + "\n");
+        uno.jouer();
+    }
+
+    public void jouer(int nbbots)
+    {
+        uno.initialiser(nbbots);
+        while (!uno.getFinJeu())
+        {
+            while (!uno.getFin())
+            {
+                reagir();
+            }
+            System.out.println(uno.getNomDeJoueurCourant() + " a gagne cette main!");
+            for (Joueur j : uno.getJoueurs())
+            {
+                j.updateTotalScore();
+                j.afficherTotalScore();
+            }
+        }
+        System.out.println(uno.getNomDeJoueurCourant() + " a gagne le jeu!");
+    }
+}
