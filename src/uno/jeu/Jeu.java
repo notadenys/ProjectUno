808ede709e147f74ea21b39d6df9340498736717
notadenys <sh.denys.ev@gmail.com>
2023-12-29 13:25:55 +0100

    cree le main file de jeu

---
+++ src/uno/jeu/Jeu.java
@@ -0,0 +1,10 @@
+package uno.jeu;
+
+public class Jeu {
+    public static void main(String[] args) {
+        Uno uno = new Uno();
+        DLC dlc = new DLC(uno);
+
+        dlc.jouer(5);
+    }
+}
