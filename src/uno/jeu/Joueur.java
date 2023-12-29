763e5863cd8b9f2dddcc494e8e474638f4699dd9
notadenys <sh.denys.ev@gmail.com>
2023-12-29 17:16:56 +0100

    refactor de joueur

--- src/uno/jeu/Joueur.java
+++ src/uno/jeu/Joueur.java
@@ -1,10 +1,13 @@
 package uno.jeu;
 
+import uno.Erreurs.InvalidCoupExtension;
+import uno.cartes.Carte;
 import uno.cartes.FabriqueCartes;
 import uno.cartes.PaquetDeCartes;
 
 public abstract class Joueur {
     private String nom;
+    private int totalScore;
     private PaquetDeCartes main;
     private Uno uno;
 
@@ -17,23 +20,32 @@ public abstract class Joueur {
         main = fc.getPaquetVide();
     }
 
-    public PaquetDeCartes getMain()
+    public void updateTotalScore(){totalScore += main.getValeur();}
+
+    public void piocher()
     {
-        return main;
+        if (uno.getPioche().estVide())
+        {
+            Carte top = uno.getTopCarte();
+            uno.getTalon().retourner();
+            uno.getTalon().mettreSurTop(top);
+
+            System.out.println("Le talon a ete retourne");
+        }
+        main.ajouter(uno.getPioche().piocher());
     }
 
-    public String getNom()
-    {
-        return nom;
-    }
+    public PaquetDeCartes getMain() {return main;}
+    public String getNom() {return nom;}
+    public int getTotalScore() {return totalScore;}
     public Uno getUno() {return uno;}
 
     public boolean aMainVide() {return main.estVide();}
 
-    public void afficherRes()
+    public void afficherTotalScore()
     {
-        System.out.println(getNom() + " : " + getMain().getValeur());
+        System.out.println(nom + " : " + totalScore);
     }
 
-    abstract void jouer();
+    abstract void jouer() throws InvalidCoupExtension;
 }
